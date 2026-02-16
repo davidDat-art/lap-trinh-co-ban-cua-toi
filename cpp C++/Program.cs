dd6f24b9125a0a9fe717303568072f96853af567
davidDat-art <datxoacthao123@gmail.com>
2026-02-16 20:33:02 +0700

    C# bài 1

---
+++ cpp C++/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int S = 0;
+
+            Console.Write("Nhap n: ");
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                S = S + i;
+            }
+
+            Console.WriteLine("S = " + S);
+            Console.ReadKey();
+        }
+    }
+}
